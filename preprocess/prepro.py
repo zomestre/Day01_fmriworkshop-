ebb6e53acc7a8bf72f310be863a51f78debd6d56
gracer <gracer@dhcp-v880-0-00144.1Xwireless.unc.edu>
2017-11-14 19:01:28 -0500

    got the trimming working, now on to moco

--- preprocess/prepro.py
+++ preprocess/prepro.py
@@ -5,7 +5,7 @@ import pdb
 #import sys
 import argparse
 
-def prepro(basedir, args, arglist):
+def prepro(basedir, args, arglist, outhtml, out_bad_bold_list):
 #bet
     #print(args)
     #pdb.set_trace()
@@ -52,9 +52,37 @@ def prepro(basedir, args, arglist):
                     os.system("fslroi %s %s_trimmed %s %s"%(output, output, arglist['EX'], arglist['TOT']))
                     pdb.set_trace()
             
+#motion correction
+    if args.MOCO==False:
+        print("please set a threshold for the FD, a good one is 0.9")
+    else:
+        print("starting motion correction")
+        os.chdir(os.path.join(basedir))
+        for dir in glob.glob('sub-*/func'):
+            if not os.path.exists(os.path.join(basedir,dir,'motion_assessment')):
+                os.makedirs(os.path.join(basedir,dir,'motion_assessment'))
+                pdb.set_trace()
+            os.chdir(os.path.join(basedir, dir))
+            for input in glob.glob('*.nii.gz'):
+                output=input.strip('.nii.gz')
+                if os.path.exists(output+'_mcf.nii.gz'):
+                    print(output+' exists, skipping')
+                else:
+                    os.system("fsl_motion_outliers -i %s -o motion_assessment/confound.txt --fd --thresh=%s -p motion_assessment/fd_plot -v > motion_assessment/outlier_output.txt"%(output,arglist['MOCO']))
+                    os.system("cat motion_assessment/outlier_output.txt >> %s"%(outhtml))
+                    os.system("echo '<p>=============<p>FD plot %s <br><IMG BORDER=0 SRC=%s/motion_assess/fd_plot.png WIDTH=100%s></BODY></HTML>' >> %s"%(dir, dir,'%', outhtml))
+                    pdb.set_trace()
+ 
 
 def main():
     basedir='/Users/gracer/Google Drive/fMRI_workshop/data'
+    # I'm using a big html file to put all QA info together.  If you have other suggestions, let me know!
+    outhtml = "/Users/jeanettemumford/Documents/Research/Talks/MumfordBrainStats/ds008/Scripts/bold_motion_QA.html"
+    out_bad_bold_list = "/Users/jeanettemumford/Documents/Research/Talks/MumfordBrainStats/ds008/Scripts/subs_lose_gt_45_vol_scrub.txt"
+ 
+    os.system("rm %s"%(out_bad_bold_list))
+    os.system("rm %s"%(outhtml))
+    
     parser=argparse.ArgumentParser(description='preprocessing')
     parser.add_argument('-bet',dest='STRIP',action='store_true',
                         default=False, help='bet via fsl using defaults for functional images')
@@ -66,12 +94,12 @@ def main():
                         default=False, help='TRs to remove')
     parser.add_argument('-total',dest='TOT',
                         default=False, help='total TRs')
-    parser.add_argument('-moco',dest='MOCO',action='store_true',
+    parser.add_argument('-moco',dest='MOCO',
                         default=False, help='this is using fsl_motion_outliers to preform motion correction and generate a confounds.txt as well as DVARS')
     args = parser.parse_args()
     arglist={}
     for a in args._get_kwargs():
         arglist[a[0]]=a[1]
     print(arglist)
-    prepro(basedir, args, arglist)
+    prepro(basedir, args, arglist, outhtml, out_bad_bold_list)
 main()
