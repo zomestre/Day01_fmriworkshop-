df912ed661519efd3c12fc7de904aa440edca73a
gracer <gracer@dhcp-v880-0-00144.1Xwireless.unc.edu>
2017-11-16 17:04:23 -0500

    trying something new

--- feat_scripts/make_fsf.py
+++ feat_scripts/make_fsf.py
@@ -3,35 +3,38 @@ import glob
 import os
 from subprocess import check_output
 import pdb
+import argparse
 
-#cs154_mkC.feat
-#cs154_mkC
-#things I will need
-#Output directory
-#TR
-#Total volumes
-#Input
-#EV files
-#Structural image
-def create_fsf(basedir,repl_dict,outdir):
+#confounds CONFOUNDS
+#evs EV1
+
+def create_fsf(basedir,repl_dict,outdir, arglist):
     os.chdir(basedir)
     for sub in glob.glob('sub-*'):
         repl_dict.update({'SUB':sub})
         print(sub)
-        for scan in glob.glob(os.path.join(sub,'func','*_mcf.nii.gz')):
-            pdb.set_trace()
+        for scan in glob.glob(os.path.join(sub,'func','%s_task-%s_bold_brain_mcf.nii.gz')%(sub,arglist['TASK'])):
             print(scan)
-            task=scan.split('/')[2].split('_')[1].split('-')[1]
+            #task=scan.split('/')[2].split('_')[1].split('-')[1]
             funcrun=os.path.join(basedir,scan)
             repl_dict.update({'FUNCRUN':funcrun})
             ntmpts=check_output(['fslnvols',funcrun])
             repl_dict.update({'NTIMEPOINTS':ntmpts})
             trs=check_output(['fslval','%s'%(funcrun),'pixdim4',scan])
             repl_dict.update({'TRS':trs})
-            output=os.path.join(outdir,sub,task)
+            output=os.path.join(outdir,sub,arglist['TASK'])
             repl_dict.update({'OUTPUT':output})
-            anat=os.path.join(basedir,sub,'anat','%s_T1w.nii.gz'%(sub))
+            anat=os.path.join(basedir,sub,'anat','%s_T1w_brain.nii.gz'%(sub))
             repl_dict.update({'ANAT':anat})
+            print arglist['EV']
+            #pdb.set_trace()
+            ctr=0
+            for item in arglist['EV']:
+                print(item)
+                ctr=ctr+1
+                ev=os.path.join(basedir,sub,'func','onsets','%s_%s_%soutput.txt'%(sub,arglist['TASK'],item))
+                repl_dict.update({'EV%i'%ctr:ev})
+            pdb.set_trace()
             print(repl_dict)
             with open(os.path.join(basedir,'design.fsf'),'r') as infile:
                 tempfsf=infile.read()
@@ -44,6 +47,17 @@ def create_fsf(basedir,repl_dict,outdir):
 def main ():
   basedir='/Users/gracer/Desktop/data'
   outdir=os.path.join(basedir,'derivatives','task')
+  parser=argparse.ArgumentParser(description='making fsf files')
+  parser.add_argument('-task',dest='TASK',
+                        default=False, help='which task are we using?')
+  parser.add_argument('-evs',dest='EV',nargs='+',
+                        default=False, help='which evs are we using?')
+
   repl_dict={}
-  create_fsf(basedir,repl_dict, outdir)
+  args = parser.parse_args()
+  arglist={}
+  for a in args._get_kwargs():
+      arglist[a[0]]=a[1]
+      print(arglist)
+  create_fsf(basedir,repl_dict, outdir, arglist)
 main()
