7fcb9ea780ca275e43027807683a6c3bc8c0e9ff
gracer <gracer@dhcp-v880-0-00144.1Xwireless.unc.edu>
2017-11-16 12:20:31 -0500

    decided to make it a function with inputs

--- onset_parser.py
+++ onset_parser.py
@@ -5,78 +5,55 @@
 import numpy as np
 import glob
 import os
-#import fnmatch
 import pdb
 import pandas
+import argparse
 
-basepath='/Users/gracer/Desktop/data'
-os.chdir(basepath)
+def ons_parse(basepath,args, arglist,writedir):
+    os.chdir(basepath)
+    print arglist['COLS']
+    pdb.set_trace()  
+    for ons_file in glob.glob('sub-*/func/sub-*bart_events.tsv'):
+        print("starting %s"%ons_file)
+        sub=ons_file.split('/')[0]
+        if os.path.exists(os.path.join(basepath, sub, 'func','onsets'))==False:
+            os.makedirs(os.path.join(basepath, sub, 'func','onsets'))
+        x=pandas.read_csv(ons_file, sep='\t')
+#        header=x.columns.tolist()
+        var=arglist['COLS']
+#        print(var)
+#        pdb.set_trace()
+        use=x[['onset','duration','%s'%var]]
+        print(use)
+        pdb.set_trace()
+#        a = np.array(header)
+#        RT = [0, 1, 2]
+#        RTcols=list(a[RT])
+#        print(header)
+        pdb.set_trace() 
+        x.to_csv(os.path.join(writedir,'%s_%s_output.csv'%(sub,arglist['COLS'])), columns = use, index=False, header=False)
+        
+        pdb.set_trace()        
 
-onsets=[]
-duration=[]
-RT=[]
-trialT=[]
-action=[]
-for ons_file in glob.glob('sub-*/func/sub-*bart_events.tsv'):
-    print("starting %s"%ons_file)
-    sub=ons_file.split('/')[0]
-    if os.path.exists(os.path.join(basepath, sub, 'func','onsets'))==False:
-        os.makedirs(os.path.join(basepath, sub, 'func','onsets'))
-    x=pandas.read_csv(ons_file, sep='\t')
-    header = ["onset", "duration", "reaction_time"]
-    x.to_csv('~/Desktop/trash/%s_output.csv'%sub, columns = header, index=False, header=False)
-    pdb.set_trace()        
-#    onsets.append(x.iloc[:,0])
-#    duration.append(x.iloc[:,1])
-#    RT.append(x.iloc[:,2])
-#    trialT.append(x.iloc[:,4])
-#    action.append(x.iloc[:,6])
-#    with open(ons_file,'r') as f:
-#        for line in f.readlines()[:]:
-#            print("HI GRACE")
-#            l_s=line.strip().split('\t')
-#            onsets.append(l_s[0])
-#            duration.append(l_s[1])
-#            RT.append(l_s[2])
-#            trialT.append(l_s[4])
-#            action.append(l_s[6])
-#    filename=ons_file.split('/')[2].split('.')[0]
-#    print(filename)
-#    ons_array=np.array(onsets)
-#    pdb.set_trace()
-#    print ons_array
-#    pdb.set_trace()
-#    baloon=np.zeros(ons_array.size)
-#    print(baloon)
-#    pdb.set_trace()
-#    baloon[np.array(trialT)=='BALOON']='1'
-#    baloon[np.array(trialT)=='CONTROL']='0'
-#    cash=np.array(onsets)
-#    cash[np.array(action)=='ACCEPT']='0'
-#    cash[np.array(action)=='CASHOUT']='1'
-#    cash[np.array(action)=='EXPLODE']='-1'
-#    meanrt=np.mean(RT)
-#    finalrt=RT-meanrt
-#    
-#  
-#    f_RT=open(os.path.join(basepath, sub, 'func','onsets','ons_RT_'+filename+'.txt'), 'w')
-#    f_TT=open(os.path.join(basepath, sub, 'func','onsets','ons_TT_'+filename+'.txt'), 'w')
-#    f_ACT=open(os.path.join(basepath, sub, 'func','onsets','ons_ACT_'+filename+'.txt'), 'w')
-#    
-#    for t in range(len(onsets)):
-#        f_RT.write('%f\t%f\t%f\n' %(onsets[t],duration[t],finalrt[t]))
-#        f_ACT.write('%f\t%f\t%f\n' %(onsets[t],duration[t],cash[t]))
-#        f_TT.write('%f\t%f\t%f\n' %(onsets[t],duration[t],baloon[t]))
-#        
-#
-#    f_RT.close()    
-#    f_TT.close()
-#    f_ACT.close()
-#    pdb.set_trace()
-#    
+
+def main():
+    basepath='/Users/gracer/Desktop/data'
+    writedir='/Users/gracer/Desktop/trash'
+    
+
+    parser=argparse.ArgumentParser(description='onset parser, expecting a tsv file')
+    parser.add_argument('-cols',dest='COLS',
+                        default=False, help='which columns do you want? Each file will contain the onset, duration, and the column of choice. You can list multiple columns')
+    args = parser.parse_args()
+    arglist={}
+    for a in args._get_kwargs():
+        arglist[a[0]]=a[1]
+    print(arglist)
+    ons_parse(basepath, args, arglist,writedir)    
+main()
 
     
     
 
 
-
+#%cd /Users/gracer/Google\ Drive/fMRI_workshop/scripts/
