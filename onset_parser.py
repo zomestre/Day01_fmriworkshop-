d95c217ae04fa80ea735de2173d3c168e6d6e43c
gracer <gracer@dhcp-v880-0-00144.1Xwireless.unc.edu>
2017-11-17 12:12:17 -0500

    holy shit i think it is working

--- onset_parser.py
+++ onset_parser.py
@@ -5,11 +5,11 @@
 
 import glob
 import os
-import pdb
+#import pdb
 import pandas
 import argparse
   
-def ons_parse(basepath,args, arglist):
+def ons_parse(basepath,args, arglist,missing):
     os.chdir(basepath)
     print arglist['COLS']
     for item in arglist['COLS']:
@@ -18,34 +18,31 @@ def ons_parse(basepath,args, arglist):
             sub=ons_file.split('/')[0]
             if os.path.exists(os.path.join(basepath, sub, 'func','onsets'))==False:
                 os.makedirs(os.path.join(basepath, sub, 'func','onsets'))
-            x=pandas.read_csv(ons_file, sep='\t')
+            x=pandas.read_csv(ons_file, sep='\t', na_values=missing['miss'])
             use=x[['onset','duration','%s'%item]]
             print(use[item].dtype) 
             if use[item].dtype == 'float64' or use[item].dtype == 'int64':
-                print("this looks like a number, lets demean it")
+                print("%s %s looks like a number, lets demean it"%(sub,use[item]))
                 meanie=use[item].mean()
                 use[item]=use[item] - meanie
             else:
-                print("this looks categorical, let's change it to 0s and 1s")
+                print("%s %s looks categorical, let's change it to 0s and 1s"%(sub,use[item]))
                 use.iloc[:,2]=use.iloc[:,2].astype('category')
                 varbs=use.iloc[:,2].unique()
-                pdb.set_trace()
+                
                 for name in varbs:
                     save_name=os.path.join(basepath,sub,'func','onsets','%s_%s_%s_%s_output.txt'%(sub,arglist['TASK'],item,name))
                     print(save_name)
-                    pdb.set_trace()
                     tosave=use.loc[use.iloc[:,2] == name]
-                    pdb.set_trace()
+                    
                     def score_to_numeric(x):
                         if x== name:
                             return 1
                     tosave.iloc[:,2] = tosave.iloc[:,2].apply(score_to_numeric)
-                    pdb.set_trace()
+                    
                     tosave.to_csv(save_name, sep=" " ,index=False, header=False)
-                pdb.set_trace()
-                use[item] = pandas.get_dummies(use[item])
-                use.to_csv(os.path.join(basepath,sub,'func','onsets','%s_%s_%s_output.txt'%(sub,arglist['TASK'],item)), sep=" " ,index=False, header=False)
                 
+                #use.to_csv(os.path.join(basepath,sub,'func','onsets','%s_%s_%s_output.txt'%(sub,arglist['TASK'],item)), sep=" " ,index=False, header=False)
             print(use)
             use.to_csv(os.path.join(basepath,sub,'func','onsets','%s_%s_%s_output.txt'%(sub,arglist['TASK'],item)), sep=" " ,index=False, header=False)
                
@@ -60,12 +57,15 @@ def main():
                         default=False, help='which task would you like to parse?')
     parser.add_argument('-cols',dest='COLS',nargs='+',
                         default=False, help='which columns do you want? Each file will contain the onset, duration, and the column of choice. You can list multiple columns')
+    MISS=['NA','N/A','n/a']
+    missing={'miss':MISS}
     args = parser.parse_args()
     arglist={}
+    
     for a in args._get_kwargs():
         arglist[a[0]]=a[1]
     print(arglist)
-    ons_parse(basepath, args, arglist)    
+    ons_parse(basepath, args, arglist,missing)    
 main()
 
     
