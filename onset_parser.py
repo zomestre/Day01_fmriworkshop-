50fabc3c470e81d391e3555cbd3e185e43f80566
gracer <gracer@dhcp-v880-0-00144.1Xwireless.unc.edu>
2017-11-17 10:47:31 -0500

    got it to seperate the categorical variables into unique txt files

--- onset_parser.py
+++ onset_parser.py
@@ -5,10 +5,10 @@
 
 import glob
 import os
-#import pdb
+import pdb
 import pandas
 import argparse
-
+  
 def ons_parse(basepath,args, arglist):
     os.chdir(basepath)
     print arglist['COLS']
@@ -20,24 +20,39 @@ def ons_parse(basepath,args, arglist):
                 os.makedirs(os.path.join(basepath, sub, 'func','onsets'))
             x=pandas.read_csv(ons_file, sep='\t')
             use=x[['onset','duration','%s'%item]]
-            print(use[item].dtype)
-#            pdb.set_trace()  
+            print(use[item].dtype) 
             if use[item].dtype == 'float64' or use[item].dtype == 'int64':
                 print("this looks like a number, lets demean it")
                 meanie=use[item].mean()
                 use[item]=use[item] - meanie
             else:
                 print("this looks categorical, let's change it to 0s and 1s")
+                use.iloc[:,2]=use.iloc[:,2].astype('category')
+                varbs=use.iloc[:,2].unique()
+                pdb.set_trace()
+                for name in varbs:
+                    save_name=os.path.join(basepath,sub,'func','onsets','%s_%s_%s_%s_output.txt'%(sub,arglist['TASK'],item,name))
+                    print(save_name)
+                    pdb.set_trace()
+                    tosave=use.loc[use.iloc[:,2] == name]
+                    pdb.set_trace()
+                    def score_to_numeric(x):
+                        if x== name:
+                            return 1
+                    tosave.iloc[:,2] = tosave.iloc[:,2].apply(score_to_numeric)
+                    pdb.set_trace()
+                    tosave.to_csv(save_name, sep=" " ,index=False, header=False)
+                pdb.set_trace()
                 use[item] = pandas.get_dummies(use[item])
+                use.to_csv(os.path.join(basepath,sub,'func','onsets','%s_%s_%s_output.txt'%(sub,arglist['TASK'],item)), sep=" " ,index=False, header=False)
+                
             print(use)
             use.to_csv(os.path.join(basepath,sub,'func','onsets','%s_%s_%s_output.txt'%(sub,arglist['TASK'],item)), sep=" " ,index=False, header=False)
-        
-#            pdb.set_trace()        
+               
 
 
 def main():
     basepath='/Users/gracer/Desktop/data'
-#    writedir='/Users/gracer/Desktop/trash'
     
 
     parser=argparse.ArgumentParser(description='onset parser, expecting a tsv file')
@@ -57,3 +72,4 @@ main()
     
 
 
+#%cd /Users/gracer/Google\ Drive/fMRI_workshop/scripts/
