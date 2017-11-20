23b00b1608cf167e3626fbeac79cf97c15474a5e
gracer <gracer@dhcp-v880-0-00144.1Xwireless.unc.edu>
2017-11-20 15:49:22 -0500

    including no reg option

--- feat_scripts/make_fsf.py
+++ feat_scripts/make_fsf.py
@@ -23,7 +23,7 @@ def create_fsf(basedir,repl_dict,outdir, arglist):
         trs=check_output(['fslval','%s'%(funcrun),'pixdim4',scan])
         repl_dict.update({'TRS':trs})
         output=os.path.join(outdir,sub,arglist['TASK'])
-        repl_dict.update({'OUTPUT':output})
+        repl_dict.update({'OUTPUT':output})            
         anat=os.path.join(basedir,sub,'anat','%s_T1w_brain.nii.gz'%(sub))
         repl_dict.update({'ANAT':anat})
         print arglist['EV']
@@ -41,20 +41,34 @@ def create_fsf(basedir,repl_dict,outdir, arglist):
             repl_dict.update({'MOTCOR%i'%i:motcor})
         pdb.set_trace()
         print(repl_dict)
-        with open(os.path.join(basedir,'design.fsf'),'r') as infile:
-            tempfsf=infile.read()
-            for key in repl_dict:
-                tempfsf = tempfsf.replace(key, repl_dict[key])
-                with open(os.path.join(outdir,sub,'%s_%s.fsf'%(sub,arglist['TASK'])),'w') as outfile:
-                    outfile.write(tempfsf)
-                outfile.close()
-        infile.close()
+        if arglist['NOREG']==False:
+            with open(os.path.join(basedir,'design.fsf'),'r') as infile:
+                tempfsf=infile.read()
+                for key in repl_dict:
+                    tempfsf = tempfsf.replace(key, repl_dict[key])
+                    with open(os.path.join(outdir,sub,'%s_%s.fsf'%(sub,arglist['TASK'])),'w') as outfile:
+                        outfile.write(tempfsf)
+                    outfile.close()
+                infile.close()
+        else:
+            print("skipping registration")
+            with open(os.path.join(basedir,'no_reg_design.fsf'),'r') as infile:
+                tempfsf=infile.read()
+                for key in repl_dict:
+                    tempfsf = tempfsf.replace(key, repl_dict[key])
+                    with open(os.path.join(outdir,sub,'%s_%s.fsf'%(sub,arglist['TASK'])),'w') as outfile:
+                        outfile.write(tempfsf)
+                    outfile.close()
+                infile.close()
+
                        
 
 def main ():
   basedir='/Users/gracer/Desktop/data'
   outdir=os.path.join(basedir,'derivatives','task')
   parser=argparse.ArgumentParser(description='making fsf files')
+  parser.add_argument('-noreg',dest='NOREG', action='store_true',
+                        default=False, help='Did you already register your data (using ANTZ maybe)?')
   parser.add_argument('-task',dest='TASK',
                         default=False, help='which task are we using?')
   parser.add_argument('-evs',dest='EV',nargs='+',
