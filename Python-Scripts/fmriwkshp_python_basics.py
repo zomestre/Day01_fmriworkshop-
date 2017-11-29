cccf9ecb12730ea50ece4e621df5369bd6af82c9
NikkiBytes <nicholletteacosta@gmail.com>
2017-11-29 05:24:37 +0000

    Initial Python Basic scripts

---
+++ Python-Scripts/fmriwkshp_python_basics.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 17:48:40 2017
+@author: Nichollette
+
+This is a basic script for the fMRI workshop Python Basic Lesson
+This is how you do a block comment
+"""
+
+# This is how you do individual comments
+
+
+variableI = 100 # This is an Integer 
+ 
+variableF= 100.30 # This is a Float 
+ 
+variableB = True # This is a Boolean 
+ 
+variableS = "fMRI Workshop 2017" # This is a String
+ 
+print("Integer: %d\n" % (variableI) +  "Float:  %d\n" % (variableF) 
+  + "Boolean:  %s\n" %(variableB) + "String: %s" %(variableS))  
+
+dict = {'Name' : 'Leo', 'Species': 'Cat', 'Age': 7} # dictionary
+
+print("dict['Name']:", dict['Name'], "\ndict['Species']:", dict['Species'], "\ndict['Age']):", dict['Age'])
+print(dict)
+
+dict['Age'] = 8; # update Dictionary
+dict['BloodType'] = 'A' 
+
+
+print("Updated dict['Age']", dict['Age']) # Add new entry
+print(dict)
+
+
+
+
